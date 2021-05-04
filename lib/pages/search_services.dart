78628e26b4c80eaf8c14d1663098527d9c472743
Karol Blanco <kmblanco@uninorte.edu.co>
2021-05-04 17:11:02 -0500

    search_services

--- lib/pages/search_services.dart
+++ lib/pages/search_services.dart
@@ -11,7 +11,9 @@ class Search extends StatefulWidget {
 class _SearchState extends State<Search> {
   var paramSearch;
   var iconSelected;
-  List<IconData> _icons=[
+
+
+   List<IconData> _icons=[
     FontAwesomeIcons.chalkboardTeacher,
     FontAwesomeIcons.paintRoller,
     FontAwesomeIcons.wrench,
@@ -19,7 +21,7 @@ class _SearchState extends State<Search> {
     FontAwesomeIcons.dumbbell,
   ];
 
-  Widget _buildIcon(int index){
+   Widget _buildIcon(int index){
     return GestureDetector(
         onTap: (){
           setState(() {
@@ -45,135 +47,231 @@ class _SearchState extends State<Search> {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
       home: Scaffold(
-
-        appBar: AppBar(
-          leading: Column(
-            mainAxisAlignment: MainAxisAlignment.start,
-            children: [
-              IconButton(
-                icon: Icon(Icons.home_outlined,size: 35,color: Colors.white,),
-                onPressed: (){
-                     Navigator.of(context).pushNamed("/home");
-                   }
-                ),
-            ],
-          ),
-          actions: <Widget>[
-            Column(
+          appBar: AppBar(
+            leading: Column(
               mainAxisAlignment: MainAxisAlignment.start,
               children: [
                 IconButton(
-                  icon: Icon(Icons.account_circle_outlined,size: 35,color: Colors.white,),
-                  onPressed: (){
-                     Navigator.of(context).pushNamed("/profile");
-                   }
-                   ),
+                    icon: Icon(
+                      Icons.home_outlined,
+                      size: 35,
+                      color: Colors.white,
+                    ),
+                    onPressed: () {
+                      Navigator.of(context).pushNamed("/home");
+                    }),
               ],
-            )
-          ],
-          backgroundColor: Color(int.parse("#41736C".replaceAll('#', '0xff'))),
-          toolbarHeight: 200.0,
-          shape: RoundedRectangleBorder(borderRadius: BorderRadius.only(bottomRight: Radius.circular(67),bottomLeft: Radius.circular(67))),
-          title: Column(
-            children: [
-              Text('SEARCH',
-                style: TextStyle(fontSize: 25 ),
-              ),
-              Container(
-                margin: EdgeInsets.only(top:40),
-                child: Card(
-                elevation: 20.0,
-                shape: RoundedRectangleBorder(
-                  borderRadius: BorderRadius.all(Radius.circular(20.0)),
+            ),
+            actions: <Widget>[
+              Column(
+                mainAxisAlignment: MainAxisAlignment.start,
+                children: [
+                  IconButton(
+                      icon: Icon(
+                        Icons.account_circle_outlined,
+                        size: 35,
+                        color: Colors.white,
+                      ),
+                      onPressed: () {
+                        Navigator.of(context).pushNamed("/profile");
+                      }),
+                ],
+              )
+            ],
+            backgroundColor:
+                Color(int.parse("#41736C".replaceAll('#', '0xff'))),
+            toolbarHeight: 200.0,
+            shape: RoundedRectangleBorder(
+                borderRadius: BorderRadius.only(
+                    bottomRight: Radius.circular(67),
+                    bottomLeft: Radius.circular(67))),
+            title: Column(
+              children: [
+                Text(
+                  'SEARCH',
+                  style: TextStyle(fontSize: 25),
                 ),
-                child: Padding(
-                  padding: EdgeInsets.symmetric(horizontal: 15.0),
-                  child: Row(
-                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                    children: <Widget>[
-                      GestureDetector(
-                        child: Icon(
-                          Icons.search,
-                          color: Colors.black54,
-                        ),
-                        onTap: () {
-                          search();
-                        },
+                Container(
+                  margin: EdgeInsets.only(top: 40),
+                  child: Card(
+                    elevation: 20.0,
+                    shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.all(Radius.circular(20.0)),
+                    ),
+                    child: Padding(
+                      padding: EdgeInsets.symmetric(horizontal: 15.0),
+                      child: Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                        children: <Widget>[
+                          GestureDetector(
+                            child: Icon(
+                              Icons.search,
+                              color: Colors.black54,
+                            ),
+                            onTap: () {
+                              search();
+                            },
+                          ),
+                          SizedBox(
+                            width: 10.0,
+                          ),
+                          Expanded(
+                            child: TextField(
+                              decoration: InputDecoration(
+                                  border: InputBorder.none,
+                                  hintText: "Search..."),
+                              onChanged: (value) {
+                                setState(() {
+                                  paramSearch = value;
+                                });
+                              },
+                            ),
+                          ),
+                        ],
                       ),
-                      SizedBox(
-                        width: 10.0,
-                      ),
-                      Expanded(
-                        child: TextField(
-                          decoration: InputDecoration(
-                              border: InputBorder.none, hintText: "Search..."),
-                          onChanged: (value) {
-                            setState(() {
-                              paramSearch = value;
-                            });
-                          },
-                        ),
-                      ),
-                     
-                    ],
+                    ),
                   ),
                 ),
-        ),
-              ),
-            ],
+              ],
+            ),
           ),
-        ),
-        body: Column(
-          children: [
-            Container(
-              margin: EdgeInsets.only(top:10,bottom: 10),
-              height:100.0,
-              child: new ListView.builder(
-                itemCount: _icons.length,
-                itemBuilder: (context, index){
-                return _buildIcon(index);
-              },
-              scrollDirection: Axis.horizontal,
-              )
+          body: Column(
+            children: [
+              Container(
+                  margin: EdgeInsets.only(top: 10, bottom: 10),
+                  height: 100.0,
+                  child: new ListView.builder(
+                    itemCount: _icons.length,
+                    itemBuilder: (context, index) {
+                      return _buildIcon(index);
+                    },
+                    scrollDirection: Axis.horizontal,
+                  )),
+              Text(
+                'SERVICES',
+                style: TextStyle(fontSize: 25),
+              ),
+              Container(
+                width: 350,
+                height: 300,
+                child: FutureBuilder<QuerySnapshot>(
+                    future:
+                        FirebaseFirestore.instance.collection('services').get(),
+                    builder: (context, snapshot) {
+                      if (snapshot.hasData) {
+                        final List<DocumentSnapshot> documents =
+                            snapshot.data.docs;
+                        return ListView(
 
-            ),
+                            //Service card
+                            children: documents
+                                .map((doc) => Card(
+                                      child: Container(
+                                        color: Color(int.parse(
+                                            "#E6EEED".replaceAll('#', '0xff'))),
+                                        height: 200,
+                                        child: Row(
+                                          children: [
+                                            Expanded(
+                                              child: Stack(
+                                                children: [
+                                                  Align(
+                                                    child: ClipRRect(
+                                                        borderRadius:
+                                                            BorderRadius
+                                                                .circular(8.0),
+                                                        child: Image.asset(
+                                                            'assets/images/plomero.jpg')),
+                                                  )
+                                                ],
+                                              ),
+                                            ),
+                                            Expanded(
+                                              child: Stack(
+                                                children: [
+                                                  Container(
+                                                    margin: EdgeInsets.only(
+                                                        top: 60, bottom: 20),
+                                                    decoration: BoxDecoration(
+                                                        color: Color(int.parse(
+                                                            "#E6EEED"
+                                                                .replaceAll('#',
+                                                                    '0xff'))),
+                                                        borderRadius:
+                                                            BorderRadius.only(
+                                                                topRight: Radius
+                                                                    .circular(
+                                                                        20),
+                                                                bottomRight: Radius
+                                                                    .circular(
+                                                                        20))),
+                                                  ),
+                                                  Align(
+                                                    child: Stack(children: [
+                                                      ListTile(
+                                                        title:
+                                                            Text(doc['name']),
+                                                        subtitle: Text(
+                                                          doc['price'].toString(),
+                                                          style: TextStyle(
+                                                              fontWeight:
+                                                                  FontWeight
+                                                                      .bold),
+                                                        ),
+                                                      ),
+                                                      Container(
+                                                        margin: EdgeInsets.only(
+                                                            top: 60,
+                                                            bottom: 20),
+                                                        decoration: BoxDecoration(
+                                                            color: Color(int
+                                                                .parse("#8B9A99"
+                                                                    .replaceAll(
+                                                                        '#',
+                                                                        '0xff'))),
+                                                            borderRadius: BorderRadius.only(
+                                                                topRight: Radius
+                                                                    .circular(
+                                                                        20),
+                                                                bottomRight: Radius
+                                                                    .circular(
+                                                                        20))),
+                                                        child: ListTile(
+                                                          subtitle: Text(
+                                                            doc['description'],
+                                                            style: TextStyle(
+                                                                fontWeight:
+                                                                    FontWeight
+                                                                        .bold),
+                                                          ),
+                                                        ),
+                                                      )
+                                                    ]),
+                                                  )
+                                                ],
+                                              ),
+                                            )
+                                          ],
+                                        ),
+                                      ),
 
-            Text('SERVICES',
-            style: TextStyle(fontSize: 25),
-            ),
-              
-            Container(
-              width: 350,
-              height: 300,
-
-              child: FutureBuilder<QuerySnapshot>(
-                future: FirebaseFirestore.instance.collection('services').get(),
-                builder: (context,snapshot){
-                  if(snapshot.hasData){
-                    final List<DocumentSnapshot> documents = snapshot.data.docs;
-                    return ListView(
-                      //Service card
-                      children: documents.
-                          map((doc) => Card(
-                            color: Color(int.parse("#E6EEED".replaceAll('#', '0xff'))),
+                                      /* color: Color(int.parse("#8B9A99".replaceAll('#', '0xff'))),
                             shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(20)),
                             child: ListTile(
                               title: Text(doc['name']),
-                              subtitle: Text(doc['price'].toString(), style: TextStyle(fontWeight: FontWeight.bold),),
-                            ),
-                          )).toList()
-                    );
-                  }else if (!snapshot.hasData){
-                    return Center(child: CircularProgressIndicator(),);
-                  }
-
-                }
-                
-              ),
-            )
-          ],
-          )
-      ),
+                              subtitle: Text(doc['price'], style: TextStyle(fontWeight: FontWeight.bold),),
+                            ), */
+                                    ))
+                                .toList());
+                      } else if (!snapshot.hasData) {
+                        return Center(
+                          child: CircularProgressIndicator(),
+                        );
+                      }
+                    }),
+              )
+            ],
+          )),
     );
   }
 
@@ -183,7 +281,7 @@ class _SearchState extends State<Search> {
         .where('name', isGreaterThanOrEqualTo: paramSearch)
         .get();
     final List<DocumentSnapshot> documents = result.docs;
-    
+
     Navigator.of(context).pushNamed("/results", arguments: documents);
   }
 
