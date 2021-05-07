7f8f40541d8343ebf6723caa94925bf4ee7b77d3
Karol Blanco <kmblanco@uninorte.edu.co>
2021-05-07 17:01:30 -0500

    log in

--- test/integration_test/app_test.dart
+++ test/integration_test/app_test.dart
@@ -1,4 +1,5 @@
 import 'package:diakonia/main.dart' as app;
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 import 'package:integration_test/integration_test.dart';
@@ -8,18 +9,22 @@ group('integration test', () {
 IntegrationTestWidgetsFlutterBinding.ensureInitialized();
 testWidgets("login with correct email and password", 
 (tester) async {
-     app.main();
+
+    app.main();
+   
     await tester.pumpAndSettle();
     final emailField = find.byKey(Key("emailLogin"));
     final passwordField = find.byKey(Key("passwordLogin"));
     final signInButton = find.text("Log in");
     
-    await tester.enterText(emailField, "test@testmail.com");
-    await tester.enterText(passwordField, "123456");
-    await tester.pumpAndSettle();
+    await tester.enterText(emailField, "Brayaaan@gmail.com");
+    await tester.enterText(passwordField, "zxcasdqwe123");
+    await tester.pumpAndSettle(Duration(seconds: 5));
 
     await tester.tap(signInButton);
-    await tester.pumpAndSettle();
+    await tester.pumpAndSettle(Duration(seconds: 5));
+    
+    
 
 });
 });
